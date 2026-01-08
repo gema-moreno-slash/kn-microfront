8a576cdfc1f5f8f1fded49618d477c4c66c58c8c
Gema Moreno <gema.moreno@slashmobility.com>
2026-01-08 10:15:20 +0100

    [dev] Styles iframe

--- packages/orq-iframe/src/index.js
+++ packages/orq-iframe/src/index.js
@@ -11,21 +11,53 @@ export class AppComponent extends LitElement {
       .mainCont {
         display: flex;
         flex-direction: column;
+        min-height: 100vh;
       }
 
       .title {
         font-family: Monoton, sans-serif;
         text-align: center;
+        font-weight: normal;
+        font-size: 4rem;
+        margin: 1rem 0;
+      }
+
+      iframe {
+        border: none;
+        width: 100%;
+        height: 100%;
       }
 
       .cont {
+        flex-grow: 1;
+        margin: 1rem;
+        display: flex;
+        gap: 1rem;
+      }
+
+      .cont .examples {
+        flex: 1;
+        flex-direction: column;
+        display: flex;
+        gap: 1rem;
+      }
+
+      .cont .poke {
+        flex: 2;
         display: flex;
       }
 
-      .left, .right {
-        flex-grow: 1;
-        display: flex;
-        justify-content: center;
+      .frame {
+        border-radius: 1rem;
+        padding: 0.5rem;
+        border: 0.2rem dashed darkgray;
+        box-sizing: border-box;
+      }
+
+      footer {
+        margin: 2rem;
+        text-align: center;
+        font-weight: bold;
       }
     `
   ]
@@ -37,15 +69,17 @@ export class AppComponent extends LitElement {
   render() {
     return html`
       <div class="mainCont">
-        <h1 class="title">Pet Show</h1>
+        <h1 class="title">Microfronts</h1>
         <div class="cont">
-          <div class="left">
-            <iframe src="http://localhost:3011"></iframe>
+          <div class="examples">
+            <iframe class="frame" src="http://localhost:3011"></iframe>
+            <iframe class="frame" src="http://localhost:3012"></iframe>
           </div>
-          <div class="right">
-            <iframe src="http://localhost:3012"></iframe>
+          <div class="poke">
+            <iframe class="frame" src="http://localhost:5173"></iframe>
           </div>
         </div>
+        <footer>Kodeneko@2026</footer>
       </div>
     `;
   }
